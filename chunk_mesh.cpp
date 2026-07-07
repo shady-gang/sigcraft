90d38d2028580241654ae52671d9219718407bc9
Hugo Devillers <devillers@cg.uni-saarland.de>
2026-07-07 11:49:19 +0200

    rewrote chunk mesher using functional programming

--- chunk_mesh.cpp
+++ chunk_mesh.cpp
@@ -10,145 +10,65 @@ extern "C" {
 #include <assert.h>
 #include <vector>
 
-#define MINUS_X_FACE(V) \
-V(-1, -1, -1,   0, 0, -1, 0, 0) \
-V(-1,  1, -1,   0, 1, -1, 0, 0) \
-V(-1,  1,  1,   1, 1, -1, 0, 0) \
-V(-1, -1, -1,   0, 0, -1, 0, 0) \
-V(-1,  1,  1,   1, 1, -1, 0, 0) \
-V(-1, -1,  1,   1, 0, -1, 0, 0)
+using namespace nasl;
 
-#define PLUS_X_FACE(V) \
-V(1,   1, -1,   1, 1, 1, 0, 0) \
-V(1,  -1, -1,   1, 0, 1, 0, 0) \
-V(1,   1,  1,   0, 1, 1, 0, 0) \
-V(1,   1,  1,   0, 1, 1, 0, 0) \
-V(1,  -1, -1,   1, 0, 1, 0, 0) \
-V(1,  -1,  1,   0, 0, 1, 0, 0)
+using add_vertex_fn_t = void(ivec3, vec2, ivec3);
+using generate_face_fn_t = void(const std::function<add_vertex_fn_t>&);
+using add_face_fn_t = void(ivec3, vec3, const std::function<generate_face_fn_t>&);
 
-#define MINUS_Z_FACE(V) \
-V(1,   1, -1,   0, 1, 0, 0, -1) \
-V(-1, -1, -1,   1, 0, 0, 0, -1) \
-V(1,  -1, -1,   0, 0, 0, 0, -1) \
-V(-1,  1, -1,   1, 1, 0, 0, -1) \
-V(-1, -1, -1,   1, 0, 0, 0, -1) \
-V(1,   1, -1,   0, 1, 0, 0, -1)
+static auto minus_x_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(0, 0, 0), vec2(0, 0), ivec3(-1, 0, 0));
+    add_vertex(ivec3(0, 1, 0), vec2(0, 1), ivec3(-1, 0, 0));
+    add_vertex(ivec3(0, 1, 1), vec2(1, 1), ivec3(-1, 0, 0));
+    add_vertex(ivec3(0, 0, 0), vec2(0, 0), ivec3(-1, 0, 0));
+    add_vertex(ivec3(0, 1, 1), vec2(1, 1), ivec3(-1, 0, 0));
+    add_vertex(ivec3(0, 0, 1), vec2(1, 0), ivec3(-1, 0, 0));
+};
 
-#define PLUS_Z_FACE(V) \
-V(1,  -1,  1,   1, 0, 0, 0, 1) \
-V(-1, -1,  1,   0, 0, 0, 0, 1) \
-V(1,   1,  1,   1, 1, 0, 0, 1) \
-V(1,   1,  1,   1, 1, 0, 0, 1) \
-V(-1, -1,  1,   0, 0, 0, 0, 1) \
-V(-1,  1,  1,   0, 1, 0, 0, 1)
+static auto plus_x_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(1, 1, 0), vec2(1, 1), ivec3(1, 0, 0));
+    add_vertex(ivec3(1, 0, 0), vec2(1, 0), ivec3(1, 0, 0));
+    add_vertex(ivec3(1, 1, 1), vec2(0, 1), ivec3(1, 0, 0));
+    add_vertex(ivec3(1, 1, 1), vec2(0, 1), ivec3(1, 0, 0));
+    add_vertex(ivec3(1, 0, 0), vec2(1, 0), ivec3(1, 0, 0));
+    add_vertex(ivec3(1, 0, 1), vec2(0, 0), ivec3(1, 0, 0));
+};
 
-#define MINUS_Y_FACE(V) \
-V(-1, -1, -1,   0, 0, 0, -1, 0) \
-V(1,  -1,  1,   1, 1, 0, -1, 0) \
-V(1,  -1, -1,   1, 0, 0, -1, 0) \
-V(1,  -1,  1,   1, 1, 0, -1, 0) \
-V(-1, -1, -1,   0, 0, 0, -1, 0) \
-V(-1, -1,  1,   0, 1, 0, -1, 0)
+static auto minus_z_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(1, 1, 0), vec2(0, 1), ivec3(0, 0, -1));
+    add_vertex(ivec3(0, 0, 0), vec2(1, 0), ivec3(0, 0, -1));
+    add_vertex(ivec3(1, 0, 0), vec2(0, 0), ivec3(0, 0, -1));
+    add_vertex(ivec3(0, 1, 0), vec2(1, 1), ivec3(0, 0, -1));
+    add_vertex(ivec3(0, 0, 0), vec2(1, 0), ivec3(0, 0, -1));
+    add_vertex(ivec3(1, 1, 0), vec2(0, 1), ivec3(0, 0, -1));
+};
 
-#define PLUS_Y_FACE(V) \
-V(1,   1,  1,   1, 0, 0, 1, 0) \
-V(-1,  1, -1,   0, 1, 0, 1, 0) \
-V(1,   1, -1,   1, 1, 0, 1, 0) \
-V(-1,  1,  1,   0, 0, 0, 1, 0) \
-V(-1,  1, -1,   0, 1, 0, 1, 0) \
-V(1,   1,  1,   1, 0, 0, 1, 0)
+static auto plus_z_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(1, 0, 1), vec2(1, 0), ivec3(0, 0, 1));
+    add_vertex(ivec3(0, 0, 1), vec2(0, 0), ivec3(0, 0, 1));
+    add_vertex(ivec3(1, 1, 1), vec2(1, 1), ivec3(0, 0, 1));
+    add_vertex(ivec3(1, 1, 1), vec2(1, 1), ivec3(0, 0, 1));
+    add_vertex(ivec3(0, 0, 1), vec2(0, 0), ivec3(0, 0, 1));
+    add_vertex(ivec3(0, 1, 1), vec2(0, 1), ivec3(0, 0, 1));
+};
 
-#define CUBE(V) \
-MINUS_X_FACE(V) \
-PLUS_X_FACE(V)  \
-MINUS_Z_FACE(V) \
-PLUS_Z_FACE(V)  \
-MINUS_Y_FACE(V) \
-PLUS_Y_FACE(V)\
+static auto minus_y_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(0, 0, 0), vec2(0, 0), ivec3(0, -1, 0));
+    add_vertex(ivec3(1, 0, 1), vec2(1, 1), ivec3(0, -1, 0));
+    add_vertex(ivec3(1, 0, 0), vec2(1, 0), ivec3(0, -1, 0));
+    add_vertex(ivec3(1, 0, 1), vec2(1, 1), ivec3(0, -1, 0));
+    add_vertex(ivec3(0, 0, 0), vec2(0, 0), ivec3(0, -1, 0));
+    add_vertex(ivec3(0, 0, 1), vec2(0, 1), ivec3(0, -1, 0));
+};
 
-//#define V(cx, cy, cz, t, s, nx, ny, nz) tmp[0] = (int) ((cx + 1) / 2) + (float) x; tmp[1] = (int) ((cy + 1) / 2) + (float) y; tmp[2] = (int) ((cz + 1) / 2) + (float) z; tmp[3] = t; tmp[4] = s; g.push_back(tmp[0]); g.push_back(tmp[1]); g.push_back(tmp[2]); g.push_back(tmp[3]); g.push_back(tmp[4]);
-#define V(cx, cy, cz, t, s, nx, ny, nz) \
-v.vx = ((cx + 1) / 2) + x;             \
-v.vy = ((cy + 1) / 2) + y;             \
-v.vz = ((cz + 1) / 2) + z;             \
-v.tt = t * 255;             \
-v.ss = t * 255;             \
-v.nnx = nx * 127 + 128;            \
-v.nny = ny * 127 + 128;            \
-v.nnz = nz * 127 + 128;            \
-v.br = color.x * 255;            \
-v.bg = color.y * 255;            \
-v.bb = color.z * 255;            \
-add_vertex();
-
-static void paste_minus_x_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    MINUS_X_FACE(V)
-}
-
-static void paste_plus_x_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    PLUS_X_FACE(V)
-}
-
-static void paste_minus_y_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    MINUS_Y_FACE(V)
-}
-
-static void paste_plus_y_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    PLUS_Y_FACE(V)
-}
-
-static void paste_minus_z_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    MINUS_Z_FACE(V)
-}
-
-static void paste_plus_z_face(std::vector<uint8_t>& g, nasl::vec3 color, unsigned x, unsigned y, unsigned z) {
-    float tmp[5];
-    ChunkMesh::Vertex v;
-    auto add_vertex = [&](){
-        uint8_t tmp[sizeof(v)];
-        memcpy(tmp, &v, sizeof(v));
-        for (auto b : tmp)
-            g.push_back(b);
-    };
-    PLUS_Z_FACE(V)
-}
-
-#undef V
+static std::function<generate_face_fn_t> plus_y_face = [](const std::function<add_vertex_fn_t>& add_vertex) {
+    add_vertex(ivec3(1, 1, 1), vec2(1, 0), ivec3(0, 1, 0));
+    add_vertex(ivec3(0, 1, 0), vec2(0, 1), ivec3(0, 1, 0));
+    add_vertex(ivec3(1, 1, 0), vec2(1, 1), ivec3(0, 1, 0));
+    add_vertex(ivec3(0, 1, 1), vec2(0, 0), ivec3(0, 1, 0));
+    add_vertex(ivec3(0, 1, 0), vec2(0, 1), ivec3(0, 1, 0));
+    add_vertex(ivec3(1, 1, 1), vec2(1, 0), ivec3(0, 1, 0));
+};
 
 static BlockData access_safe(const ChunkData* chunk, ChunkNeighborsUnsafe& neighbours, int x, int y, int z) {
     unsigned int i, k;
@@ -181,8 +101,7 @@ static BlockData access_safe(const ChunkData* chunk, ChunkNeighborsUnsafe& neigh
     return BlockAir;
 }
 
-void chunk_mesh(const ChunkData* chunk, ChunkNeighborsUnsafe& neighbours, std::vector<uint8_t>& g, size_t* num_verts) {
-    *num_verts = 0;
+void traverse_chunk_mesh(const ChunkData* chunk, ChunkNeighborsUnsafe& neighbours, const std::function<add_face_fn_t>& add_face) {
     for (int section = 0; section < CUNK_CHUNK_SECTIONS_COUNT; section++) {
         for (int x = 0; x < CUNK_CHUNK_SIZE; x++)
             for (int y = 0; y < CUNK_CHUNK_SIZE; y++)
@@ -190,39 +109,51 @@ void chunk_mesh(const ChunkData* chunk, ChunkNeighborsUnsafe& neighbours, std::v
                     int world_y = y + section * CUNK_CHUNK_SIZE;
                     BlockData block_data = access_safe(chunk, neighbours, x, world_y, z);
                     if (block_data != BlockAir) {
-                        nasl::vec3 color;
+                        vec3 color;
                         color.x = block_colors[block_data].r;
                         color.y = block_colors[block_data].g;
                         color.z = block_colors[block_data].b;
                         if (access_safe(chunk, neighbours, x, world_y + 1, z) == BlockAir) {
-                            paste_plus_y_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, plus_y_face);
                         }
                         if (access_safe(chunk, neighbours, x, world_y - 1, z) == BlockAir) {
-                            paste_minus_y_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, minus_y_face);
                         }
 
                         if (access_safe(chunk, neighbours, x + 1, world_y, z) == BlockAir) {
-                            paste_plus_x_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, plus_x_face);
                         }
                         if (access_safe(chunk, neighbours, x - 1, world_y, z) == BlockAir) {
-                            paste_minus_x_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, minus_x_face);
                         }
 
                         if (access_safe(chunk, neighbours, x, world_y, z + 1) == BlockAir) {
-                            paste_plus_z_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, plus_z_face);
                         }
                         if (access_safe(chunk, neighbours, x, world_y, z - 1) == BlockAir) {
-                            paste_minus_z_face(g, color, x, world_y, z);
-                            *num_verts += 6;
+                            add_face(ivec3(x, world_y, z), color, minus_z_face);
                         }
                     }
                 }
     }
+}
+
+auto make_vertex_encoder(const ivec3& block_position, const vec3& color) {
+    return [&](ivec3 position, vec2 uv, ivec3 normal) {
+        ChunkMesh::Vertex v;
+        v.vx = block_position.x + position.x;
+        v.vy = block_position.y + position.y;
+        v.vz = block_position.z + position.z;
+        v.tt = uv.x * 255;
+        v.ss = uv.y * 255;
+        v.nnx = normal.x * 127 + 128;
+        v.nny = normal.y * 127 + 128;
+        v.nnz = normal.z * 127 + 128;
+        v.br = color.x * 255;
+        v.bg = color.y * 255;
+        v.bb = color.z * 255;
+        return v;
+    };
 }
 
 ChunkMesh::ChunkMesh(imr::Device& d, ChunkNeighbors& n) {
@@ -233,7 +164,21 @@ ChunkMesh::ChunkMesh(imr::Device& d, ChunkNeighbors& n) {
             unsafe.neighbours[x][z] = &n.neighbours[x][z].get()->data;
         }
     }
-    chunk_mesh(unsafe.neighbours[1][1], unsafe, g, &num_verts);
+
+    num_verts = 0;
+    std::function<add_face_fn_t> add_face = [&](ivec3 block_position, vec3 color, const std::function<generate_face_fn_t>& gen_face) {
+        auto encoder = make_vertex_encoder(block_position, color);
+        std::function<add_vertex_fn_t> add_vertex = [&](ivec3 position, vec2 uv, ivec3 normal){
+            ChunkMesh::Vertex v = encoder(position, uv, normal);
+            uint8_t tmp[sizeof(v)];
+            memcpy(tmp, &v, sizeof(v));
+            for (auto b : tmp)
+                g.push_back(b);
+            num_verts += 1;
+        };
+        gen_face(add_vertex);
+    };
+    traverse_chunk_mesh(unsafe.neighbours[1][1], unsafe, add_face);
 
     //fprintf(stderr, "%zu vertices, totalling %zu KiB of data\n", num_verts, num_verts * sizeof(float) * 5 / 1024);
     //fflush(stderr);
